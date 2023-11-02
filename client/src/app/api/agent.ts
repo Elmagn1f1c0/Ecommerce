4dc5e14eccd71d77e1346e6c7d7d019bef3a9346
Elmagn1f1c0 <steveamadi007@gmail.com>
2023-11-03 00:41:42 +0100

    Pagination

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,13 +1,21 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
+import { PaginatedResponse } from "../models/pagination";
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-axios.interceptors.response.use(response =>{
+axios.interceptors.response.use(response => {
+    
+
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginatedResponse(response.data, JSON.parse(pagination));
+        return response;
+    }
     return response
 }, (error: AxiosError) => {
     const {data, status} = error.response as AxiosResponse;
@@ -75,3 +83,5 @@ const agent = {
     Basket
 }
 export default agent;
+
+
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,9 +2,12 @@ import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import ProductList from "./ProductList";
 import { useEffect } from "react";
-import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, Typography } from "@mui/material";
+import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
+import { Grid, Paper } from "@mui/material";
 import ProductSearch from "./ProductSearch";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
+import CheckboxButtons from "../../app/components/CheckboxButtons";
+import AppPagination from "../../app/components/AppPagination";
 
 const sortOptions = [
   {value: 'name', label: 'Alphabetical'},
@@ -14,7 +17,7 @@ const sortOptions = [
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status, filtersLoaded, brands, types } = useAppSelector(state => state.catalog);
+    const {productsLoaded, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
     
 
@@ -26,7 +29,7 @@ export default function Catalog() {
     if (!filtersLoaded) dispatch(fetchFilters());
   }, [dispatch, filtersLoaded])
 
-  if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
+  if (!filtersLoaded) return <LoadingComponent message='Loading products...' />
   
     return (
       <Grid container spacing={4}>
@@ -34,48 +37,40 @@ export default function Catalog() {
           <Paper sx={{mb: 2}}>
             <ProductSearch />
           </Paper>
+
           <Paper sx={{mb: 2, p: 2}}>
-          <FormControl component="fieldset">
-            <RadioGroup>
-              {sortOptions.map(({value, label}) => (
-                <FormControlLabel value={value} control={<Radio/>} label={label} key={value} />
-              ))}
-            </RadioGroup>
-          </FormControl>
+            <RadioButtonGroup 
+              selectedValue={productParams.orderBy} 
+              options={sortOptions}
+              onChange={(e) => dispatch(setProductParams({orderBy: e.target.value}))}
+            />
+          </Paper>
+          <Paper sx={{mb: 2, p: 2}}>
+              <CheckboxButtons 
+                items={brands}
+                checked={productParams.brands}
+                onChange={(items: string[]) => dispatch(setProductParams({brands: items}))}
+              />
           </Paper>
 
           <Paper sx={{mb: 2, p: 2}}>
-            <FormGroup >
-              {brands.map(brand => (
-                <FormControlLabel control={<Checkbox  />} label={brand} key={brand} />
-              ))}
-            </FormGroup>
-          </Paper>
-
-          <Paper sx={{mb: 2, p: 2}}>
-            <FormGroup >
-              {types.map(type => (
-                <FormControlLabel control={<Checkbox  />} label={type} key={type} />
-              ))}
-            </FormGroup>
+          <CheckboxButtons 
+                items={types}
+                checked={productParams.types}
+                onChange={(items: string[]) => dispatch(setProductParams({types: items}))}
+              />
           </Paper>
         </Grid>
         <Grid item xs={9}>
           <ProductList products={products}/>
         </Grid>
         <Grid item xs={3} />  
-        <Grid item xs={9}>
-          <Box display='flex' justifyContent='space-between' alignItems='center'>
-                <Typography>
-                  Displaying 1-6 of 20 items
-                </Typography>
-                <Pagination 
-                  color='secondary'
-                  size='large'
-                  count={10}
-                  page={2}
-                />
-          </Box>
+        <Grid item xs={9} sx={{mb:2}}>
+          {metaData && 
+          <AppPagination 
+            metaData={metaData}
+            onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))}
+          />}
         </Grid>
       </Grid>
     )
