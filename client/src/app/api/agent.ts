07a0b88d2e78fdc166d07c9020bd3bab1faa2fcb
Elmagn1f1c0 <steveamadi007@gmail.com>
2023-11-08 03:35:29 +0100

    Stripe Payment completed

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -93,12 +93,17 @@ const Orders = {
     create: (values: any) => requests.post('orders', values)
 }
 
+const  Payments = {
+     CreatePaymentIntent: () => requests.post('payments', {})
+}
+
 const agent = {
     Catalog,
     TestErrors,
     Basket,
     Account,
-    Orders
+    Orders,
+    Payments
 }
 export default agent;
 
